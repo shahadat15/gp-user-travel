8b9617c1cb71920cf7ee3b345023576ecbd4a55c
Shahadat Iqbal <9406700+shahadat15@users.noreply.github.com>
2022-10-28 11:29:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,40 +9,31 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
+    # Load tower informaiton
     inputFileTower = open('tower_list.csv', 'r')
     tower = defaultdict(int)
     line = inputFileTower.readline()
     line = inputFileTower.readline()
     while line:
         fields = line.split(",")
-        location = (float(fields[1].strip("\'")),float(fields[2].strip("\'\\\n")))
-        tower[location]=int(fields[0].strip("\'"))
+        location = [float(fields[1].strip("\'")),float(fields[2].strip("\'\\\n"))]
+        tower[int(fields[0].strip("\'"))]=location
         line = inputFileTower.readline()
-    inputFileTower.close()  
-    
+
+    inputFileTower.close() 
+
     # Accessing API data using the python "requests" module
     oneUser = pd.read_csv('inputFile.csv')
-    date = 624
-    oneDayOneUser = oneUser.loc[oneUser["date"] == date]
-    tower_filtered = set(oneDayOneUser["tower"].unique())
-
-    lat = []
-    lon = []
-    name = []
-    for key,value in tower.items():
-        if value in tower_filtered:
-            name.append(value)
-            lat.append(key[0])
-            lon.append(key[1])
-    data = pd.DataFrame({'lat':lat,'lon':lon,'name':name}, dtype=str)
+    oneUser['lat'] =  oneUser.apply (lambda row: tower[row.tower][0], axis=1)
+    oneUser['lon'] =  oneUser.apply (lambda row: tower[row.tower][1], axis=1)
+    data = oneUser
     # Make an empty map
-    folium_map = folium.Map(location=[23.8139, 90.3986], tiles="OpenStreetMap", zoom_start=10)
-    for i in range(0,len(data)):
-        folium.Marker(
-        location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-        popup=data.iloc[i]['name'],).add_to(folium_map)
-
-    return folium_map._repr_html_()
+    m = folium.Map(location=[23.8139, 90.3986], tiles="OpenStreetMap", zoom_start=12)
+    for index, row in data.iterrows():
+        folium.CircleMarker([row['lat'], row['lon']],
+                        radius=15,fill_color=row['date'],fill=True, popup = (row)).add_to(m)
+    m                                                           
+    return m._repr_html_()
 
 if __name__ == "__main__":
     app.run()
